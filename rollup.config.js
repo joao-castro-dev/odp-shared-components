d060cbcac296fd6df42d89394be88aca2523443c
rodrigoFreitasAcctgit <rodrigo.freitas@acct.global>
2025-02-10 16:08:20 -0300

    fix: fixed rollup file

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
-import resolve from "rollup-plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 import fs from "fs";
 import path from "path";
 import pkg from "./package.json";
-import typescript from "@rollup/plugin-typescript";
 
 const components = fs
     .readdirSync("./src/components")
@@ -14,36 +15,39 @@ const components = fs
         fs.statSync(path.join("./src/components", name)).isDirectory()
     );
 
-export default components.map((component) => ({
-    input: `src/components/${component}/index.ts`,
-    output: [
-        {
-            file: `dist/components/${component}/index.ts`,
-            format: "cjs",
-            exports: "named",
-        },
-        {
-            file: `dist/components/${component}/index.es.ts`,
-            format: "es",
-            exports: "named",
-        },
-    ],
-    plugins: [
-        postcss({
-            modules: true,
-        }),
-        external(),
-        babel({
-            exclude: "node_modules/**",
-        }),
-        resolve(),
-        commonjs(),
-        typescript({ tsconfig: "./tsconfig.json" }),
-    ],
-    external: Object.keys(pkg.peerDependencies || {}),
-},
-{
-    input: "src/index.ts",
-    output: [{ file: packgeJson.types }],
-    plugins: [deepStrictEqual.defaut()],
-}));
+export default [
+    ...components.map((component) => ({
+        input: `src/components/${component}/index.tsx`,
+        output: [
+            {
+                file: `dist/components/${component}/index.js`,
+                format: "cjs",
+                exports: "named",
+            },
+            {
+                file: `dist/components/${component}/index.es.js`,
+                format: "es",
+                exports: "named",
+            },
+        ],
+        plugins: [
+            postcss({
+                modules: true,
+            }),
+            external(),
+            babel({
+                babelHelpers: "bundled",
+                exclude: "node_modules/**",
+            }),
+            resolve(),
+            commonjs(),
+            typescript({ tsconfig: "./tsconfig.json" }),
+        ],
+        external: Object.keys(pkg.peerDependencies || {}),
+    })),
+    {
+        input: "src/index.ts",
+        output: [{ file: pkg.types, format: "es" }],
+        plugins: [dts()],
+    },
+];
